213b383e09267347229beaefde706a922bcbeaec
Hedrav Bands <60276490+hedravBands@users.noreply.github.com>
2020-11-26 04:24:52 -0500

    Add files via upload
    Test with SliverStaggeredGrid to display products gracefully.

---
+++ screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:minimart/tab/home_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        HomeTab(),
+
+      ],
+    );
+  }
+}
